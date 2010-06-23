ba55f95f4ef92d776327686ec9f8b0adee64d9a6
Luke Gallagher <notfornoone@gmail.com>
2010-06-23 22:50:57 +1000

    License details

--- mute-screamer.php
+++ mute-screamer.php
@@ -8,6 +8,30 @@ Version: 0.1
 Author URI: http://notfornoone.com/
 */
 
+/*
+ * Mute Screamer
+ *
+ * Copyright (c) 2010 Luke Gallagher
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 if( !version_compare(PHP_VERSION, '5.1.6', '>=') ) {
 	exit('Mute Screamer requires PHP 5.1.6 or higher to run. You are currently running PHP ' . PHP_VERSION . '.');
 }
@@ -17,9 +41,6 @@ if( !version_compare($wp_version, '2.9', '>=') ) {
 	exit('Mute Screamer requires Wordpress 2.9.x or higher to run. You are currently running Wordpress ' . $wp_version . '.');
 }
 
-/**
- * Mute Screamer
- */
 if( !class_exists('Mute_screamer')) {
 	define( 'MSCR_PATH', dirname(__FILE__) );
 	set_include_path( get_include_path() . PATH_SEPARATOR . MSCR_PATH . '/lib' );
@@ -27,6 +48,9 @@ if( !class_exists('Mute_screamer')) {
 	require_once 'IDS/Log/Composite.php';
 	require_once 'IDS/Log/Database.php';
 
+	/**
+	 * Mute Screamer
+	 */
 	class Mute_screamer {
 		const INTRUSIONS_TABLE = 'mscr_intrusions';
 		private $options = array();
