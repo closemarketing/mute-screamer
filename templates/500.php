bb5fe3d524798a3976da1b8b3b8b6043ab31faf3
Luke Gallagher <notfornoone@gmail.com>
2010-08-31 22:25:05 +1000

    Add 500 error template

---
+++ templates/500.php
@@ -0,0 +1,26 @@
+<?php  if ( ! defined('ABSPATH') ) exit;
+/**
+ * The template for displaying 500 error pages (Server Error).
+ */
+
+get_header(); ?>
+
+	<div id="container">
+		<div id="content" role="main">
+
+			<div id="post-0" class="post error500 server-error">
+				<h1 class="entry-title"><?php _e( 'An Error Was Encountered', 'twentyten' ); ?></h1>
+				<div class="entry-content">
+					<p><?php _e( 'Apologies, there was an error with the page you requested. Perhaps searching will help.', 'twentyten' ); ?></p>
+					<?php get_search_form(); ?>
+				</div><!-- .entry-content -->
+			</div><!-- #post-0 -->
+
+		</div><!-- #content -->
+	</div><!-- #container -->
+	<script type="text/javascript">
+		// focus on search field after it has loaded
+		document.getElementById('s') && document.getElementById('s').focus();
+	</script>
+
+<?php get_footer(); ?>
