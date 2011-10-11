f57eec4bf266e078af33da4f070d4df4023c3344
Luke Gallagher <notfornoone@gmail.com>
2011-10-11 14:26:56 +1100

    Remove delete from mscr_intrusions_per_page tests

--- tests/MuteScreamer/MSCRUtilsTest.php
+++ tests/MuteScreamer/MSCRUtilsTest.php
@@ -10,7 +10,6 @@ class MSCRUtilsTest extends WP_UnitTestCase {
 	}
 
 	public function testIntrusionsPerPage()	{
-		delete_option('mscr_intrusions_per_page');
 		$this->assertEquals(20, MSCR_Utils::mscr_intrusions_per_page(), '->mscr_intrusions_per_page default is 20');
 	}
 }
