7df8f80db3c4fabd7ec86cb956cbd20c132625e3
Luke Gallagher <notfornoone@gmail.com>
2011-10-14 11:00:47 +1100

    Add get, post, server tests

--- tests/MuteScreamer/MSCRUtilsTest.php
+++ tests/MuteScreamer/MSCRUtilsTest.php
@@ -13,6 +13,31 @@ class MSCRUtilsTest extends WP_UnitTestCase {
 		$this->assertEquals(20, MSCR_Utils::mscr_intrusions_per_page(), '->mscr_intrusions_per_page default is 20');
 	}
 
+	public function testGet() {
+		$_GET = array();
+
+		$this->assertFalse(MSCR_Utils::get('foo'), '->get returns false for non-existent value');
+
+		$_GET['foo'] = 'bar';
+		$this->assertEquals('bar', MSCR_Utils::get('foo'));
+	}
+
+	public function testPost() {
+		$_POST = array();
+
+		$this->assertFalse(MSCR_Utils::post('foo'), '->post returns false for non-existent value');
+
+		$_POST['foo'] = 'bar';
+		$this->assertEquals('bar', MSCR_Utils::post('foo'));
+	}
+
+	public function testServer() {
+		$this->assertFalse(MSCR_Utils::server('foo'), '->server returns false for non-existent value');
+
+		$_SERVER['foo'] = 'bar';
+		$this->assertEquals('bar', MSCR_Utils::server('foo'));
+	}
+
 	public function testIsWPLogin() {
 		$_SERVER['REQUEST_URI'] = '/wp-login.php';
 		$this->assertTrue(MSCR_Utils::is_wp_login(), '->wp_is_login is true without query args');
